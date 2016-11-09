69c663e96d58ef70173165f66d3ccc8a09f1e79d
hykwtakumin <hykwtakumin@gmail.com>
2016-11-10 07:53:55 +0900

    develop on desktop

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -6,13 +6,33 @@ console.log('\'Allo \'Allo! Event Page');
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     var postedMessage = details.requestBody.formData.status.toString();
+    var repID = details.requestBody.formData.in_reply_to_status_id.toString();
+    var repToken = details.requestBody.formData.authenticity_token.toString();
     console.log(postedMessage);
+    console.log(repID);
 
-    chrome.storage.sync.set({'message': postedMessage}, function() {
-      console.log('Message Posted!');
-      new Notification('POST_pone', { tag: 'dev', body: '次のツイートを取り置きました\n' + postedMessage });
-    });
+    new Notification('POST_pone', { tag: 'dev', body: '次のツイートを取り置きました\n' + postedMessage });
+    getTweetData(repID, postedMessage);
     return {cancel: true};
   },
-  {urls: ['*://twitter.com/i/tweet/create*']},
+  {urls: ['*://twitter.com/i/tweet/create*','*://twitter.com/i/tweet/retweet*']},
   ['requestBody','blocking']);
+
+function getTweetData(repID, postedMessage) {
+
+  var purl = 'http://localhost:3000/api/posttweet';
+
+  fetch(purl, {
+    method: 'POST',
+    // mode: 'cors',
+    body: new FormData(
+      FormData.append('repID', repID),
+      FormData.append('ptweets', postedMessage),
+      // FormData.append('reptoken', repToken)
+    )
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    console.log(json);
+  });
+}
