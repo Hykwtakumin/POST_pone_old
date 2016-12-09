476508b38af04588639c6ea569cc299f96c11cef
hykwtakumin <hykwtakumin@gmail.com>
2016-12-10 06:19:14 +0900

    査読者を実際に選べるようになった (DMのため要相互フォロー)

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,9 +1,12 @@
-
-$(function () {
-  $('#sendSettings').submit(function () {
-    let clientID = uuid.v1();
-      localStorage['clientID'] = clientID;
-      localStorage['sendName'] = $('#sendName').val();
-      console.log('settings confirmed!');
-  });
-});
+window.onload = function() {
+  const form = document.forms.sendSettings;
+  let button = document.getElementById('button');
+  console.log('settings confirmed!');
+// alert('査読者を設定しました!');
+  // 背景色を赤くするイベント
+  button.addEventListener('click', function() {
+    localStorage['sendName'] = form.sendName.value;
+    console.log(localStorage['sendName']);
+    new Notification('POST_pone', { tag: 'dev', body: '次の人にTweetを見てもらいます。\n' +'@' + localStorage['sendName'] + 'さん'});
+  }, false);
+};
